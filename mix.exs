e750ef9c7b9944f6249d2fa00683ed662f51c0c6
Teemu Harju <teemu.harju@gmail.com>
2015-10-22 09:10:26 +0300

    Added package info for publishing to hex.pm.

--- mix.exs
+++ mix.exs
@@ -3,34 +3,39 @@ defmodule Memcache.Client.Mixfile do
 
   def project do
     [app: :memcache_client,
-     version: "0.0.1",
+     version: "1.0.0",
      elixir: "~> 1.0",
+     description: description,
+     package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger, :poolboy],
      mod: {Memcache.Client, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [
       {:poison, "~> 1.5.0"},
       {:poolboy, "~> 1.5.1"}
     ]
   end
+
+  defp description do
+    """
+    Memcache client library utilizing the memcache binary protocol.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Teemu Harju"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/tsharju/memcache_client"}
+    ]
+  end
+  
 end
